a4c0a90a57e51f85828f99637edce066f3ccec47
“dennohkim” <kimathidennis38@gmail.com>
2022-11-25 15:17:24 +0300

    fetch avatars

--- src/components/IssueItem.jsx
+++ src/components/IssueItem.jsx
@@ -4,9 +4,15 @@ import { relativeDate } from "../helpers/relativeDate";
 import { useUserData } from "../helpers/useUserData";
 
 export function IssueItem({
-  title, number, assignee, commentCount, createdBy, createdDate, labels, status,
+  title,
+  number,
+  assignee,
+  commentCount,
+  createdBy,
+  createdDate,
+  labels,
+  status,
 }) {
-
   const assigneeUser = useUserData(assignee);
   const createdByUser = useUserData(createdBy);
 
@@ -29,10 +35,21 @@ export function IssueItem({
           ))}
         </span>
         <small>
-          #{number} opened {relativeDate(createdDate)} by {createdByUser.isSuccess ? `by ${createdByUser.data.name}`: " "}
+          #{number} opened {relativeDate(createdDate)} by{" "}
+          {createdByUser.isSuccess ? `by ${createdByUser.data.name}` : " "}
         </small>
       </div>
-      {assignee ? <div>{assignee}</div> : null}
+      {assignee ? (
+        <img
+          src={
+            assigneeUser.isSuccess ? assigneeUser.data.profilePictureUrl : ""
+          }
+          className="assigned-to"
+          alt={`Assigned to ${
+            assigneeUser.isSuccess ? assigneeUser.data.name : "avatar"
+          }`}
+        />
+      ) : null}
       <span className="comment-count">
         {commentCount > 0 ? (
           <>
