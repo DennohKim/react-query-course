b07cbb4e08d613ebf184fec4236bfbbee4b0b346
“dennohkim” <kimathidennis38@gmail.com>
2023-01-02 14:21:20 +0300

    feat: load comments

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -1,5 +1,7 @@
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
+import { relativeDate } from "../helpers/relativeDate";
+import { useUserData } from "../helpers/useUserData";
 import { IssueHeader } from "./IssueHeader";
 
 function useIssueData(issueNumber) {
@@ -8,9 +10,41 @@ function useIssueData(issueNumber) {
   });
 }
 
+function useIssueComments(issueNumber) {
+  return useQuery(["issues", issueNumber, "comments"], async () => {
+    const res = await fetch(`/api/issues/${issueNumber}/comments`);
+    return await res.json();
+  });
+}
+
+function Comment({ comment, createdBy, createdDate }) {
+  const userQuery = useUserData(createdBy);
+
+  if (userQuery.isLoading)
+    return (
+      <div className="comment">
+        <div className="comment-header">Loading...</div>
+      </div>
+    );
+
+  return (
+    <div className="comment">
+      <img src={userQuery.data.profilePictureUrl} alt="Commenter Avatar" />
+      <div>
+        <div className="comment-header">
+          <span>{userQuery.data.name}</span> commented{" "}
+          <span>{relativeDate(createdDate)}</span>
+        </div>
+        <div className="comment-body">{comment}</div>
+      </div>
+    </div>
+  );
+}
+
 export default function IssueDetails() {
   const { number } = useParams();
   const issueQuery = useIssueData(number);
+  const commentsQuery = useIssueComments(number);
 
   return (
     <div className="issue-details">
@@ -19,6 +53,17 @@ export default function IssueDetails() {
       ) : (
         <>
           <IssueHeader {...issueQuery.data} />
+          <main>
+            <section>
+              {commentsQuery.isLoading ? (
+                <p>Loading comments...</p>
+              ) : (
+                commentsQuery.data?.map((comment) => (
+                  <Comment key={comment.id} {...comment} />
+                ))
+              )}
+            </section>
+          </main>
         </>
       )}
     </div>
